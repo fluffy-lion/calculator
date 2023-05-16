09895a48c5b11cb356a8ca845e637f314358c246
fluffy-lion <fluffylionprojects@gmail.com>
2023-05-16 19:53:39 +0100

    added input

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import Display from "./components/display/Display"
 import Wrapper from "./components/wrapper/Wrapper"
 import ButtonWrapper from "./components/buttonWrapper/ButtonWrapper"
 import Button from "./components/button/Button"
+
+const buttonValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+const removeSpaces = (num) => num.toString().replace(/\s/g, "")
+
+const math = (a, b, sign) => 
+sign === "+"
+? a + b
+: sign === "-"
+? a - b
+: sign === "X"
+? a * b
+: a / b
+
 const App = () => {
   let [calc, setCalc] = useState({
     sign: "",
     num: 0,
     res: 0
   })
-  const buttonValues = [
-    ["C", "+-", "%", "/"],
-    [7, 8, 9, "X"],
-    [4, 5, 6, "-"],
-    [1, 2, 3, "+"],
-    [0, ".", "="],
-  ];
+
 
   const numClickHandler = (e) => {
     e.preventDefault()
     const value = e.target.innerHTML
 
-    if(calc.num.length < 16){
+    if(removeSpaces(calc.num).length < 16){
       setCalc({
         ...calc,
         num: 
@@ -59,21 +75,18 @@ const App = () => {
 
   const equalsClickHandler = (e) => {
     if(calc.sign && calc.num){
-      const math = (a, b, sign) => 
-        sign === "+"
-        ? a + b
-        : sign === "-"
-        ? a - b
-        : sign === "X"
-        ? a * b
-        : a / b
+ 
 
       setCalc({
         ...calc,
         res: 
         calc.num === "0" && calc.sign === "/"
           ? "can't divide with 0"
-          : math(Number(calc.res), Number(calc.num), calc.sign),
+          : toLocaleString(math(
+            Number(removeSpaces(calc.res)),
+            Number(removeSpaces(calc.num)),
+            calc.sign
+          )),
           sign: "",
           num : 0
       })
@@ -83,15 +96,15 @@ const App = () => {
   const invertClickHandler = () => {
     setCalc({
       ...calc,
-      num: calc.num ? calc.num * -1 : 0,
-      res: calc.res ? calc.res * -1 : 0,
+      num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
       sign: ""
     })
   }
 
   const percentClickHandler = () => {
-    let num = calc.num ? parseFloat(calc.num) : 0
-    let res = calc.res ? parseFloat(calc.res) : 0
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0
 
     setCalc({
       ...calc,
@@ -109,6 +122,9 @@ const App = () => {
       res: 0
     })
   }
+
+  
+
   return (
     <div>
       <Wrapper>
