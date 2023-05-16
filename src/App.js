43556bb4961089d5576e6b06e21d93548d835a20
fluffy-lion <fluffylionprojects@gmail.com>
2023-05-16 19:40:38 +0100

    handler functions added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,145 @@
 import { useState } from "react"
+import Display from "./components/display/Display"
+import Wrapper from "./components/wrapper/Wrapper"
+import ButtonWrapper from "./components/buttonWrapper/ButtonWrapper"
+import Button from "./components/button/Button"
 const App = () => {
-  const [display, setDisplay] = useState("")
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0
+  })
+  const buttonValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+  ];
 
+  const numClickHandler = (e) => {
+    e.preventDefault()
+    const value = e.target.innerHTML
+
+    if(calc.num.length < 16){
+      setCalc({
+        ...calc,
+        num: 
+          calc.num === 0 && value === "0"
+          ? "0"
+          : calc.num % 1 === 0
+          ? Number(calc.num + value)
+          : calc.num + value,
+          res: !calc.sign ? 0 : calc.res
+      })
+    }
+  }
+
+  const commaClickHandler = (e) => {
+    e.preventDefault()
+    const value = e.target.innerHTML
+
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num
+    })
+  }
+
+  const signClickHandler = (e) => {
+    e.preventDefault()
+    const value = e.target.innerHTML
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num : 0
+    })
+  }
+
+  const equalsClickHandler = (e) => {
+    if(calc.sign && calc.num){
+      const math = (a, b, sign) => 
+        sign === "+"
+        ? a + b
+        : sign === "-"
+        ? a - b
+        : sign === "X"
+        ? a * b
+        : a / b
+
+      setCalc({
+        ...calc,
+        res: 
+        calc.num === "0" && calc.sign === "/"
+          ? "can't divide with 0"
+          : math(Number(calc.res), Number(calc.num), calc.sign),
+          sign: "",
+          num : 0
+      })
+    }
+  }
+
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: ""
+    })
+  }
+
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(calc.num) : 0
+    let res = calc.res ? parseFloat(calc.res) : 0
+
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: ""
+    })
+  }
+
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0
+    })
+  }
   return (
     <div>
-      <h1>calculator app</h1>
+      <Wrapper>
+        <Display value={calc.num ? calc.num : calc.res} />
+        <ButtonWrapper>
+          {buttonValues.flat().map((button, i) => {
+            return (
+            <Button 
+              key={i}
+              className={button === "=" ? "equals" : ""}
+              value={button}
+              onClick={
+                button === "C"
+                ? resetClickHandler
+                : button === "+-"
+                ? invertClickHandler
+                : button === "%"
+                ? percentClickHandler
+                : button === "="
+                ? equalsClickHandler
+                : button === "/" || button === "X" || button === "-" || button === "+"
+                ? signClickHandler
+                : button === "."
+                ? commaClickHandler
+                : numClickHandler
+              }
+            />
+            )
+          })}
+        </ButtonWrapper>
+      </Wrapper>
     </div>
     )
 }
